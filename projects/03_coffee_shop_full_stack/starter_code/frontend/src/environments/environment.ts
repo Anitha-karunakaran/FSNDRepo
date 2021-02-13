0bdb3d2893e1e9b034ef4ee73da6a8b1c5491ed3
Anitha Karunakaran <anitha.karunakaran@gmail.com>
2021-02-13 22:37:55 +0530

    frontend changes

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'fsndanikaruna', // the auth0 domain prefix
+    audience: 'drinks', // the audience set for the auth0 app
+    clientId: 'VVOw9idyOqt7Bw562RiiPguNvzqN2UKP', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100/tabs/user-page', // the base url of the running ionic application.
   }
 };
